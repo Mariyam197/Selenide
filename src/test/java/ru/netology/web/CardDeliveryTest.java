199a0c0ac61fc17ab74e0fe809e58bc974acf4d2
Mariam Kazaryan <mariyam.k.077@gmail.com>
2023-02-07 20:09:31 +0300

    new tests

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -2,25 +2,44 @@ package ru.netology.web;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
+    String[] monthName = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
+    String date;
+    String month;
+    String day;
+
     private String generateDate(int addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
-    @Test
-    void shouldSendFormSuccessful() {
+    @BeforeEach
+    void setUp() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.DAY_OF_MONTH, 7);
+        date = new SimpleDateFormat("dd.MM.yyyy").format(calendar.getTime());
+        month = monthName[calendar.get(Calendar.MONTH)] + " " + calendar.get(Calendar.YEAR);
+        day = Integer.toString(calendar.get(Calendar.DATE));
+    }
+
+    @Test
+    void shouldSendFormSuccessful() {
         $("[data-test-id=city] input").setValue("Москва");
         String currentDate = generateDate(3, "dd.MM.yyyy");
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
@@ -32,8 +51,25 @@ public class CardDeliveryTest {
         $(".notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
-
     }
 
+    @Test
+    void shouldSendFormSuccessfulV2() {
+        $("[data-test-id=city] input").setValue("Ка");
+        $$(".popup__content div").find(Condition.exactText("Казань")).click();
+        $("[data-test-id=date] button").click();
+
+        while (!$(".calendar__name").getText().equals(month)) {
+            $$(".calendar__arrow.calendar__arrow_direction_right").get(1).click();
+        }
+        $$("table.calendar__layout td").find(Condition.text(day)).click();
+        $("[data-test-id=name] input").setValue("Иван Иванов");
+        $("[data-test-id=phone] input").setValue("+79998887777");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + date));
+    }
 
 }
